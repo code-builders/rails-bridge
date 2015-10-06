8935fbf22e257292d91f870780ee8b8fc6f56cdb
Bookis Smuin <bookis.smuin@gmail.com>
2015-10-06 10:16:19 -0700

    add bad code

--- railsbrige/app/models/job.rb
+++ railsbrige/app/models/job.rb
@@ -1,2 +1,35 @@
 class Job < ActiveRecord::Base
+  def blah
+    (1..10).each do |i|
+      puts i
+      (1..10).each do |n|
+        puts n
+      end
+    end
+
+
+
+    puts "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "With one really long big line. With one really long big line. With one really long big line. With one really long big line. With one really long big line. With one really long big line. With one really long big line. "
+  end
+
+  def empty_method
+  end
+
+  def repetitive
+    (1..10).each do |i|
+      puts i
+      (1..10).each do |n|
+        puts n
+      end
+    end
+  end
 end
