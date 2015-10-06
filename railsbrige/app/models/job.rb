aeae383a3b0a12bfb202b017eb8126d95dd8faae
Bookis Smuin <bookis.smuin@gmail.com>
2015-10-06 10:17:24 -0700

    add really bad code

--- railsbrige/app/models/job.rb
+++ railsbrige/app/models/job.rb
@@ -32,4 +32,169 @@ class Job < ActiveRecord::Base
       end
     end
   end
+  def blah
+    (1..10).each do |i|
+      puts i
+      (1..10).each do |n|
+        puts n
+      end
+    end
+
+
+
+    puts "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "With one really long big line. With one really long big line. With one really long big line. With one really long big line. With one really long big line. With one really long big line. With one really long big line. "
+  end
+
+  def empty_method
+  end
+
+  def repetitive
+    (1..10).each do |i|
+      puts i
+      (1..10).each do |n|
+        puts n
+      end
+    end
+  end
+  def blah
+    (1..10).each do |i|
+      puts i
+      (1..10).each do |n|
+        puts n
+      end
+    end
+
+
+
+    puts "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "With one really long big line. With one really long big line. With one really long big line. With one really long big line. With one really long big line. With one really long big line. With one really long big line. "
+  end
+
+  def empty_method
+  end
+
+  def repetitive
+    (1..10).each do |i|
+      puts i
+      (1..10).each do |n|
+        puts n
+      end
+    end
+  end
+  def blah
+    (1..10).each do |i|
+      puts i
+      (1..10).each do |n|
+        puts n
+      end
+    end
+
+
+
+    puts "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "With one really long big line. With one really long big line. With one really long big line. With one really long big line. With one really long big line. With one really long big line. With one really long big line. "
+  end
+
+  def empty_method
+  end
+
+  def repetitive
+    (1..10).each do |i|
+      puts i
+      (1..10).each do |n|
+        puts n
+      end
+    end
+  end
+  def blah
+    (1..10).each do |i|
+      puts i
+      (1..10).each do |n|
+        puts n
+      end
+    end
+
+
+
+    puts "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "With one really long big line. With one really long big line. With one really long big line. With one really long big line. With one really long big line. With one really long big line. With one really long big line. "
+  end
+
+  def empty_method
+  end
+
+  def repetitive
+    (1..10).each do |i|
+      puts i
+      (1..10).each do |n|
+        puts n
+      end
+    end
+  end
+  def blah
+    (1..10).each do |i|
+      puts i
+      (1..10).each do |n|
+        puts n
+      end
+    end
+
+
+
+    puts "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "This is a long repetitive method" +
+    "With one really long big line. With one really long big line. With one really long big line. With one really long big line. With one really long big line. With one really long big line. With one really long big line. "
+  end
+
+  def empty_method
+  end
+
+  def repetitive
+    (1..10).each do |i|
+      puts i
+      (1..10).each do |n|
+        puts n
+      end
+    end
+  end
 end
