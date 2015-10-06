f63157802c52d098940e97e0d60a8909676d814d
Bookis Smuin <bookis.smuin@gmail.com>
2015-10-06 15:34:21 -0700

    Add job board project

---
+++ job-baord/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JobBaord::Application.config.secret_token = 'f8fae4d69d0e9058d43b669f1823a11e14b5d934922bd2e080bc13ff68ab104aeb7f34d3bb8eb2f9e0e2dc435aa3f5887591defbd85687bd6cfe6fde90828dfb'
